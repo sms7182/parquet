5a69ac1e6640988e90b96b512239a0c96b9e795c
FatalError <sms7182@gmail.com>
2026-05-09 15:32:49 +0330

    head with csv export

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env::args;
-
+use csv::Writer;
 use arrow::compute::kernels::{numeric, rank};
 use parquet::record::{Row, reader};
 use parquet::schema::types::Type;
@@ -8,8 +8,7 @@ use arrow::array::{Int64Array, StringArray};
 use arrow::record_batch::RecordBatch;
 use parquet::arrow::arrow_writer::ArrowWriter;
 use parquet::record::Field;
-use std::sync::Arc;
-use std::fs::File;
+use std::error::Error;
 
 fn main() {
     let args:Vec<String>=args().collect();
@@ -87,7 +86,7 @@ fn head_command(parts:&[String]){
     println!("{}",red("Error: missing file path"));
     std::process::exit(1);
 
-   }else if parts.len()>3 {
+   }else if parts.len()>5 {
        println!("{}:{}",red("Unexpected argument:"),parts[3]);
        std::process::exit(1);
    }
@@ -97,36 +96,61 @@ fn head_command(parts:&[String]){
     std::process::exit(1);
 }
    else{
-    
-    // println!("Show first {} rows from {}",green(parts[2].as_str()),green(parts[1].as_str()));
+    let mut csv_create=false;
+    let mut csv_file="";
+    if parts.len()==5{
+        let mut pairs= parts[3].split("--");
+        if let Some(space)=pairs.next() {
+             csv_create=true; 
+             if space==""{
+                if let Some(value)=pairs.next(){
+                    if value=="csv"{
+                        csv_file=parts[4].as_str();
+                    }
+                    
+                }
+                else{
+                println!("{}:{}",red("Unexpected argument:"),parts[4]);
+                }
+                
+             }
+        }
+    }
     let file=std::fs::File::open(&parts[1]).unwrap();
     let reader=SerializedFileReader::new(file).unwrap();
     let schema=reader.metadata().file_metadata().schema();
     let fields=schema.get_fields();
-    
+    let mut row_iter = reader.get_row_iter(None).expect("Failed to get row iterator");
+    let num_rows: usize=parts[2].parse().unwrap();
+    let mut vls:Vec<String>=vec![];
+
     println!("{:-<50}","");
     for field in fields{
         print!("| {} ",green(field.name()));
-        
+        vls.push(field.name().to_string());
     }
     print!("|");
     println!("");
     println!("{:-<50}","");
-    let mut row_iter = reader.get_row_iter(None).expect("Failed to get row iterator");
-    let num_rows: usize=parts[2].parse().unwrap();
-    let mut results:Vec<parquet::record::Row> = Vec::with_capacity(num_rows);
+    let mut all_rows: Vec<Vec<String>> = Vec::new();
     for _ in 0..num_rows{
         match row_iter.next() {
             Some(Ok(row))=>{
-                inspect_row_detail(&row);
+                let mut row_values=Vec::new();
+                inspect_row_detail(&row,&mut row_values);
+                all_rows.push(row_values);
                 println!("|");
                 println!("{:-<50}","");
-
-
+               
+                
             },
             Some(Err(e))=>panic!("Error reading row:{}",e),
             None=>break,
         }
+    }
+    if csv_create{
+       let mut file_name=format!("{}.csv",csv_file);
+        create_csv(&file_name, &vls,&all_rows);
     }
    }
 }
@@ -154,30 +178,40 @@ fn columns_command(parts:&[String]){
 
 
 
-fn inspect_row_detail(row:&Row){
-    for (col_name,value) in row.get_column_iter(){
+fn inspect_row_detail(row:&Row, vls:&mut Vec<String>){
+    for (_,value) in row.get_column_iter(){
         match value {
             Field::Str(s) => {
                 print!("| '{}' ", s);
+                vls.push(s.clone());
             }
             Field::Long(i) => {
                 print!("| {} ", i);
+                vls.push(i.to_string());
             }
             Field::Double(f) => {
                 print!("| {} ", f);
+                 vls.push(f.to_string());
             }
             Field::Bool(b) => {
                 print!("| {} ", b);
+                 vls.push(b.to_string());
             }
             Field::Null => {
                 print!("|  NULL value");
+                vls.push("NULL".to_string());
             }
             Field::Group(group_fields) => {
                 print!("| {} ", group_fields.len());
+                vls.push(format!("Group({})", group_fields.len()));
             }
-            _ => print!("|  Other type: {:?}", value),
+            _ => {
+                print!("|  Other type: {:?}", value);
+                vls.push(format!("{:?}", value));
+        },
         }
     }
+    
 }
 
 
@@ -190,3 +224,12 @@ fn red(text: &str) -> String {
     format!("\x1b[31m{}\x1b[0m", text)
 }
 
+fn create_csv(file_name:&str,headers:&[String],rows:&[Vec<String>])->Result<(),Box<dyn Error>>{
+    let mut wtr=Writer::from_path(file_name)?;
+    wtr.write_record(headers)?;
+    for row in rows{
+        wtr.write_record(row)?
+    }
+    wtr.flush();
+    Ok(())
+}
